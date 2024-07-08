c2aeddf12a76487f92c6b6247a2de7642d11a5e0
viwnu <xenmysafitiplace@gmail.com>
2024-07-08 16:18:34 +0300

    create video from hadler to service

--- apps/video_storage/src/features/videos/application/commands/create-video/create-video.command.handler.ts
+++ apps/video_storage/src/features/videos/application/commands/create-video/create-video.command.handler.ts
@@ -1,26 +1,16 @@
-import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 
 import { CreateVideoCommand } from './';
-import { VideoAgregate, VideoBuildResponse } from '../../../domain';
-import { VideosRepository } from '../../../infrastucture/repository';
-import { CreateVideoEvent } from '../../events/create-video';
-import { OutboxMessageType } from '../../../../../const';
+import { VideoBuildResponse } from '../../../domain';
+import { VideosService } from '../../video.service';
 
 @CommandHandler(CreateVideoCommand)
 export class CreateVideoCommandHandler implements ICommandHandler<CreateVideoCommand, VideoBuildResponse> {
   private logger = new Logger(CreateVideoCommandHandler.name);
-  constructor(
-    private readonly videosRepository: VideosRepository,
-    private readonly eventBus: EventBus,
-  ) {}
+  constructor(private readonly videosService: VideosService) {}
   async execute({ createVideoDto }: CreateVideoCommand): Promise<VideoBuildResponse> {
     this.logger.log(`Creating Video with: ${JSON.stringify(createVideoDto)}`);
-    const newVideo = VideoAgregate.create(createVideoDto);
-    newVideo.plainToInstance();
-    const message: OutboxMessageType = { ...newVideo, fileId: newVideo.id };
-    const video = await this.videosRepository.saveWithMessage(newVideo, message);
-    await this.eventBus.publish<CreateVideoEvent>(new CreateVideoEvent(video.id, video.title)); // Events dosesnt need anymore
-    return VideoAgregate.buildVideoResponse(video);
+    return await this.videosService.createVideo(createVideoDto);
   }
 }
